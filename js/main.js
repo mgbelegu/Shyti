f6142b11366e620c2d3c240c42e163bf9f1342e0
Marti Belegu <48526054+mgbelegu@users.noreply.github.com>
2021-01-20 16:17:28 +0100

    Update2

--- js/main.js
+++ js/main.js
@@ -11,4 +11,52 @@ $(function () {
   $(".navbar-nav>li>a").on("click", function () {
     $(".navbar-collapse").collapse("hide");
   });
+
+  $("#sherbim1DIV").hover(function () {
+    $("#sherbim1").slideDown("slow");
+    $("#sherbim2").slideUp("slow");
+    $("#sherbim3").slideUp("slow");
+    $("#sherbim4").slideUp("slow");
+  });
+
+  $("#sherbim2DIV").hover(function () {
+    $("#sherbim2").slideDown("slow");
+    $("#sherbim1").slideUp("slow");
+    $("#sherbim3").slideUp("slow");
+    $("#sherbim4").slideUp("slow");
+  });
+
+  $("#sherbim3DIV").hover(function () {
+    $("#sherbim3").slideDown("slow");
+    $("#sherbim2").slideUp("slow");
+    $("#sherbim1").slideUp("slow");
+    $("#sherbim4").slideUp("slow");
+  });
+
+  $("#sherbim4DIV").hover(function () {
+    $("#sherbim4").slideDown("slow");
+    $("#sherbim2").slideUp("slow");
+    $("#sherbim3").slideUp("slow");
+    $("#sherbim1").slideUp("slow");
+  });
+
+  $("html").click(function () {
+    $("#sherbim4").slideUp("slow");
+    $("#sherbim2").slideUp("slow");
+    $("#sherbim3").slideUp("slow");
+    $("#sherbim1").slideUp("slow");
+  });
+
+  $("#sherbim1DIV").click(function (event) {
+    event.stopPropagation();
+  });
+  $("#sherbim2DIV").click(function (event) {
+    event.stopPropagation();
+  });
+  $("#sherbim3DIV").click(function (event) {
+    event.stopPropagation();
+  });
+  $("#sherbim4DIV").click(function (event) {
+    event.stopPropagation();
+  });
 });
